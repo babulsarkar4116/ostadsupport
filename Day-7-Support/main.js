266877e3b6689dd4bb37020c1e4fc0bb4b17db4a
babulsarkar <babulsarkar4116@gmail.com>
2022-11-20 15:43:43 +0600

    updated

---
+++ Day-7-Support/main.js
@@ -0,0 +1,10 @@
+// array declaration
+var studentAge1=14;
+var studentAge2=10;
+var studentAge3=11;
+var studentAge4=12;
+var studentAge5=13;
+
+var studentAges=[10,11,12,13,14];
+console.log(studentAges);
+console.log(studentAges[4]);
