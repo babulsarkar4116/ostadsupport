385ccbdfaf76e5f33b200b03f6d74cfd43fe4c6d
babulsarkar <babulsarkar4116@gmail.com>
2022-11-21 11:47:00 +0600

    updated

--- Day-7-Support/main.js
+++ Day-7-Support/main.js
@@ -7,8 +7,24 @@ var studentAge5=13;
 
 var studentAges=[10,11,12,13,14,100,105,120,130];
 
+// Change
+studentAges[5]=1500;
+console.log(studentAges);
+
+
+// includes======> True or False
+// console.log(studentAges.includes(120));
+// console.log(studentAges.includes(150));
+
+// indexOf
+// if(studentAges.indexOf(120)==-1){
+//     console.log("Not found")
+// }else{
+//     console.log("Found")
+// }
+
 // Number of Place
-console.log(studentAges.indexOf(120));
+// console.log(studentAges.indexOf(120));
 
 // console.log(studentAges);
 // console.log(studentAges[4]);
