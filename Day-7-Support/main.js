b1f74ef33b5529e4340fa9338f053254c6f325d1
babulsarkar <babulsarkar4116@gmail.com>
2022-11-21 09:03:12 +0600

    updated

--- Day-7-Support/main.js
+++ Day-7-Support/main.js
@@ -6,5 +6,9 @@ var studentAge4=12;
 var studentAge5=13;
 
 var studentAges=[10,11,12,13,14];
-console.log(studentAges);
-console.log(studentAges[4]);
+// console.log(studentAges);
+// console.log(studentAges[4]);
+// console.log(studentAges[studentAges.length-4])
+// console.log("Testing========>", studentAges[studentAges.length-2]);
+console.log(studentAges.length)
+console.log("Testing=====>❤ ",studentAges.push(105));
