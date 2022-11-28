a11ee22295ce10e5fe15c202d128db649742bfac
babulsarkar <babulsarkar4116@gmail.com>
2022-11-28 15:05:43 +0600

    updated

---
+++ Day-10-Support/object.js
@@ -0,0 +1,19 @@
+// What is Object??
+
+const sakib={
+    name:"shakib al hasan",
+    age:35,
+    isActive:true,
+    isAlrounder:true,
+    country:"Bangladesh"
+}
+// Property add
+sakib.isBatter=true,
+console.log(sakib);
+
+// console.log(sakib.country)
+// console.log(`The cricketer name is ${sakib.name}, his age is ${sakib.age}, country name is ${sakib['country']} `)
+
+// Properties Name
+// console.log(Object.keys(sakib));
+// console.log(Object.values(sakib));
