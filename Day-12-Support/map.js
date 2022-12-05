03ca2bf12b5457cedffb5ad8f4bf9c2d1f608738
babulsarkar <babulsarkar4116@gmail.com>
2022-12-05 17:15:15 +0600

    updated

---
+++ Day-12-Support/map.js
@@ -0,0 +1,8 @@
+const nums=[25,24,27,37,48,58,57,69,235];
+
+// Array te je kono oparation chalano jay 
+// const doubleIt=nums.map(e=>e*2);
+// console.log("✔========",doubleIt);
+
+const doubleIt=nums.map(e=>e*2+10);
+console.log(doubleIt);
